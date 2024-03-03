948b5033c1e2258e2093de644556ac581eaef0a5
TaurenaBG <mailfortesy@gmail.com>
2024-03-03 16:06:59 +0200

    Adding MilitaryElite to repo

---
+++ MilitaryElite/LieutenantGeneral.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilitaryElite
+{
+    public class LieutenantGeneral : Soldier, ILieutenantGeneral,IPrivate
+    {
+
+        public LieutenantGeneral(string id, string firstname, string lastname, decimal salary)
+        {
+            Id = id;
+            FirstName = firstname;
+            LastName = lastname;
+            Salary = salary;
+            Privates = new();
+        }
+        public HashSet<Private> Privates { get ; set ; }
+        public decimal Salary { get ; set ; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new();
+            sb.AppendLine($"Name: {FirstName} {LastName} Id: {Id} Salary: {Salary:F2}");
+            sb.AppendLine("Privates:");
+            foreach (var item in Privates)
+            {
+                sb.AppendLine($"  {item.ToString()}");
+            }
+            return sb.ToString().TrimEnd();
+
+
+        }
+    }
+}
